e8bae9bd83d674d4848dc1ae3e60ebe54584a588
Akshun <135464796+anochronos@users.noreply.github.com>
2023-08-11 23:15:05 -0400

    Add playRound function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function getComputerChoice(){
     return choice[play];
 }
 
-function getPlayerSelection(){
+function getPlayerChoice(){
     while(true){
         let choice = parseInt(prompt("Please pick one of the following plays\n 1.Rock\n 2.Paper\n 3.Scissors"));
         switch (choice) {
@@ -20,3 +20,27 @@ function getPlayerSelection(){
     }
 }
 
+function playRound(playerSelection, computerSelection){
+    if (playerSelection===computerSelection){
+        return "Its a Tie!";
+    }
+    switch(playerSelection){
+        case "Rock":
+            if(computerSelection !== "Paper"){
+                return "You Win! Rock beats Paper";
+            }
+            else return "You Lose! Paper beats Rock";
+        
+        case "Paper":
+            if(computerSelection !== "Scissors"){
+                return "You Win! Paper Beats Rock";
+            }
+            else return "You Lose! Scissors beats Paper ";
+        
+        case "Scissors":
+            if(computerSelection !== "Rock"){
+                return "You Win! Scissors beats Paper"
+            }
+            else return "You Lose! Rock beats Scissors";
+    }
+}
