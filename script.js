f5831f7ec2225acf511130cfda850ecfa3411f88
Akshun <135464796+anochronos@users.noreply.github.com>
2023-08-11 22:55:05 -0400

    Add playerSelection function

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ function getComputerChoice(){
     const play = Math.floor(Math.random() * choice.length);
     return choice[play];
 }
+
+function getPlayerSelection(){
+    while(true){
+        let choice = parseInt(prompt("Please pick one of the following plays\n 1.Rock\n 2.Paper\n 3.Scissors"));
+        switch (choice) {
+            case 1:
+                return "Rock";
+            case 2:
+                return "Paper";
+            case 3:
+                return "Scissors";
+            default:
+                alert("Invalid Choice, Please Try Again.");
+        }
+    }
+}
+
